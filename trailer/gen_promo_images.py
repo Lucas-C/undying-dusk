6ca343f195ab829be1caacdc499fe9feb715e8db
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2021-04-27 12:35:37 +0200

    Adding undying-dusk-card.png including QR code

--- trailer/gen_promo_images.py
+++ trailer/gen_promo_images.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # INSTALL: pip install livereload
-# USAGE: PYTHONPATH=. trailer/gen_promo_images.py $id [--watch-and-rebuild]
+# USAGE: PYTHONPATH=. trailer/gen_promo_images.py [$id] [--watch-and-rebuild]
 
 import argparse, os, re, webbrowser
 from subprocess import check_output
@@ -17,25 +17,27 @@ from trailer.scenes import (chest, combat, druid, druidic_linguistics, goblin, g
 from fpdf import FPDF
 from livereload import Server
 from PIL import Image
+from qrcode import QRCode
 
 
 PARENT_DIR = os.path.dirname(__file__)
 PORT = 5500
-FILENAME_PER_ID = {
-    1: "undying-dusk-trailer1.gif",
-    2: "undying-dusk-trailer2.gif",
-    3: "undying-dusk-cover.png",
-}
 
 
 def main():
     args = parse_args()
-    out_filename = FILENAME_PER_ID[args.id]
-    sed(f"{PARENT_DIR}/index.html", 'undying-dusk-[^"]+', out_filename)
     campaign.script_it()
     fpdf = FPDF()
-    globals()[f"gen_img_{args.id}"](fpdf, out_filename)
+    if args.id:
+        name = args.id.split(".")[0]
+        globals()[f"gen_{name}"](fpdf)
+    else:
+        gen_trailer1(fpdf)
+        gen_trailer2(fpdf)
+        gen_cover(fpdf)
+        gen_card(fpdf)
     if args.watch_and_rebuild:
+        sed(f"{PARENT_DIR}/index.html", 'undying-dusk-[^"]+', f"undying-dusk-{args.id}")
         server = Server()
         cmd = ['/usr/bin/env', 'python3', __file__, str(args.id)]
         server.watch(f'{PARENT_DIR}/*.py', lambda: print(check_output(cmd).decode()))
@@ -44,11 +46,14 @@ def main():
 
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("id", type=int)
+    parser.add_argument("id", nargs="?")
     parser.add_argument("--watch-and-rebuild", action="store_true", help="Livereload GIF in a web browser")
-    return parser.parse_args()
+    args = parser.parse_args()
+    if args.watch_and_rebuild and not args.id:
+        parser.error("An image $id is required with --watch-and-rebuild")
+    return args
 
-def gen_img_1(fpdf, out_filename):
+def gen_trailer1(fpdf):
     tfc = 30  # text frames count
     # Phase 1: STORY EXPOSITION
     images = tfc * seamus(fpdf, 'My respects, my lady.') \
@@ -64,10 +69,10 @@ def gen_img_1(fpdf, out_filename):
            + 40 * [just_text(fpdf, '21 music tracks\n4 hidden secrets\nall spread over\n~150 000 PDF pages')]
     images += trailer_end(fpdf)
     images = scale(4, images)
-    images[0].save(f'{PARENT_DIR}/{out_filename}', append_images=images[1:],
+    images[0].save(f'{PARENT_DIR}/undying-dusk-trailer1.gif', append_images=images[1:],
                    save_all=True, duration=100, loop=0)
 
-def gen_img_2(fpdf, out_filename):
+def gen_trailer2(fpdf):
     # Trying to have a text frames count proportional to text length:
     images = []
     images += 20 * title_screen(fpdf, "Welcome stranger")
@@ -88,17 +93,33 @@ def gen_img_2(fpdf, out_filename):
     images += 30 * seamus(fpdf, 'ergonomics', controller=True)
     images += trailer_end(fpdf)
     images = scale(4, images)
-    images[0].save(f'{PARENT_DIR}/{out_filename}', append_images=images[1:],
+    images[0].save(f'{PARENT_DIR}/undying-dusk-trailer2.gif', append_images=images[1:],
                    save_all=True, duration=100, loop=0)
 
-def gen_img_3(fpdf, out_filename):
+def gen_cover(fpdf):
     img, pdf_img = init_pdf_img(fpdf, 'nightsky')
     bitfont_render(pdf_img, 'UNDYING DUSK', 80, 8, Justify.CENTER, size=16)
     info_render_equiplayer(pdf_img, 0, info().TYPE_ARMOR)
     info_render_equiplayer(pdf_img, 1, info().TYPE_ARMOR)
     bitfont_render(pdf_img, 'A free adventure game\nin PDF format!', 80, 90, Justify.CENTER)
     img = scale(4, [img])[0]
-    img.save(f'{PARENT_DIR}/{out_filename}')
+    img.save(f'{PARENT_DIR}/undying-dusk-cover.png')
+
+
+def gen_card(fpdf):
+    img, pdf_img = init_pdf_img(fpdf, 'nightsky')
+    bitfont_render(pdf_img, 'UNDYDING DUSK', 80, 8, Justify.CENTER, size=16)
+    info_render_equiplayer(pdf_img, 0, info().TYPE_ARMOR)
+    info_render_equiplayer(pdf_img, 1, info().TYPE_ARMOR)
+    bitfont_render(pdf_img, 'by\nLucas\nCIMON', 2, 45)
+    qrcode = QRCode(box_size=1)
+    qrcode.add_data("https://lucas-c.itch.io/undying-dusk")
+    black, white = "#140c1c", "#deeed6"
+    qr_img = qrcode.make_image(fill_color=black, back_color=white).get_image().convert('RGBA')
+    pdf_img.image(qr_img, x=120, y=49)
+    bitfont_render(pdf_img, 'A free adventure game\nin PDF format!', 80, 90, Justify.CENTER)
+    img = scale(4, [img])[0]
+    img.save(f'{PARENT_DIR}/undying-dusk-card.png')
 
 
 def scale(factor, images):
--- trailer/img_as_pdf.py
+++ trailer/img_as_pdf.py
@@ -21,33 +21,38 @@ class ImageAsPdf:
     def link(self, x, y, w, h, link, alt_text=''): pass
 
     # pylint: disable=unused-argument
-    def image(self, img_filepath, x=None, y=None, w=0, h=0, link="", title=None, alt_text=None):
-        is_bitfont = 'boxy_bold' in img_filepath
-        with Image.open(img_filepath) as img_added:
-            if self._rect_clip:
-                rc_x, rc_y, rc_width, rc_height = self._rect_clip
-                assert rc_width > 0 and rc_height > 0, f'{img_filepath}: rc_width={rc_width} rc_height={rc_height}'
-                scale = w / 516 if is_bitfont and w else 1  # retrieving bitfont scale
-                crop_x = (rc_x - x) / scale
-                crop_y = (rc_y - y) / scale
-                x, y = rc_x, rc_y
-                if x < 0:  # PIL requires x & y to be positive:
-                    if x < -rc_width: return  # out of screen, no need to render
-                    crop_x -= x
-                    x = 0
-                if y < 0:  # PIL requires x & y to be positive:
-                    if y < -rc_height: return  # out of screen, no need to render
-                    crop_y -= y
-                    y = 0
-                rc_width, rc_height = map(int, (rc_width, rc_height))  # .resize box arg must be an int tuple
-                img_added = img_added.crop((crop_x, crop_y, crop_x + rc_width / scale, crop_y + rc_height / scale))\
-                                     .resize((rc_width, rc_height), resample=Image.NEAREST)
-            else:
-                assert not (w or h), 'Not implemented yet'
-            x, y = map(int, (x, y))  # .paste box arg must be an int tuple
-            if img_added.mode == 'P':
-                img_added = img_added.convert('RGBA')
-            self.img.alpha_composite(img_added, dest=(x, y))
+    def image(self, image, *args, **kwargs):
+        if isinstance(image, str):
+            with Image.open(image) as img_added:
+                self._image(img_added, *args, **kwargs, is_bitfont='boxy_bold' in image)
+        else:
+            self._image(image, *args, **kwargs)
+
+    def _image(self, img_added, x=None, y=None, w=0, h=0, link="", title=None, alt_text=None, is_bitfont=False):
+        if self._rect_clip:
+            rc_x, rc_y, rc_width, rc_height = self._rect_clip
+            assert rc_width > 0 and rc_height > 0, f'{img_added}: rc_width={rc_width} rc_height={rc_height}'
+            scale = w / 516 if is_bitfont and w else 1  # retrieving bitfont scale
+            crop_x = (rc_x - x) / scale
+            crop_y = (rc_y - y) / scale
+            x, y = rc_x, rc_y
+            if x < 0:  # PIL requires x & y to be positive:
+                if x < -rc_width: return  # out of screen, no need to render
+                crop_x -= x
+                x = 0
+            if y < 0:  # PIL requires x & y to be positive:
+                if y < -rc_height: return  # out of screen, no need to render
+                crop_y -= y
+                y = 0
+            rc_width, rc_height = map(int, (rc_width, rc_height))  # .resize box arg must be an int tuple
+            img_added = img_added.crop((crop_x, crop_y, crop_x + rc_width / scale, crop_y + rc_height / scale))\
+                                 .resize((rc_width, rc_height), resample=Image.NEAREST)
+        else:
+            assert not (w or h), 'Not implemented yet'
+        x, y = map(int, (x, y))  # .paste box arg must be an int tuple
+        if img_added.mode == 'P':
+            img_added = img_added.convert('RGBA')
+        self.img.alpha_composite(img_added, dest=(x, y))
 
     @contextmanager
     def rect_clip(self, x, y, w, h):
