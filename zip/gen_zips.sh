f0fd73d842ffe59be9f75e31f4e794e1307a1ead
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2021-05-04 23:41:39 +0200

    Fixing publish-on-itch.io.sh

--- zip/gen_zips.sh
+++ zip/gen_zips.sh
@@ -6,7 +6,8 @@ cd $( dirname "${BASH_SOURCE[0]}" )
 
 mv ../undying-dusk.pdf .
 
-zip undying-dusk-pdf-only.zip undying-dusk.pdf
+# We include 2 files in the ZIP, otherwise butler will unzip it :(
+zip undying-dusk-pdf-only.zip undying-dusk.pdf HOW-TO-PLAY.txt
 
 wget https://kjkpub.nyc3.digitaloceanspaces.com/software/sumatrapdf/rel/SumatraPDF-3.1.2.zip
 unzip SumatraPDF-3.1.2.zip
